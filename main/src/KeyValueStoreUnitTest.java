77d1f8c9d804a8902dc27b8f572379cbd6021fbe
Saumya Gurtu <saumyagurtu@Saumyas-MacBook-Pro.local>
2021-02-13 15:48:39 +0530

    space issue fixed

--- main/src/KeyValueStoreUnitTest.java
+++ main/src/KeyValueStoreUnitTest.java
@@ -5,11 +5,11 @@ public class KeyValueStoreUnitTest {
 	@Test
    	public void getKeyValueTest() {
    	    // may use mockito for complex cases.
-   		KeyValueStore store = KeyValueStore.getInstance();
-   		store.put("a", 1, System.currentTimeMillis());
-      store.put("b", 2, System.currentTimeMillis());
-      assertEquals(1,store.get("a"));  
-      assertEquals(2,store.get("b"));
-      assertEquals(1.5,store.average()); 
-   }
+   	    KeyValueStore store = KeyValueStore.getInstance();
+            store.put("a", 1, System.currentTimeMillis());
+      	    store.put("b", 2, System.currentTimeMillis());
+      	    assertEquals(1,store.get("a"));  
+      	    assertEquals(2,store.get("b"));
+      	    assertEquals(1.5,store.average()); 
+   	}
 }
